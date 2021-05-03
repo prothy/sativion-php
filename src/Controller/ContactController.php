886db5490867962b468982727503f18e26eae7f0
prothy <lukacsurgay@gmail.com>
2021-05-03 21:36:38 +0200

    contact form now working, has some spam prevention measures on frontend (and hopefully symfony form does its part too)

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,41 @@
 namespace App\Controller;
 
 use App\Form\ContactEmailType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController {
-    public function contact(Request $request, MailerInterface $mailer): Response {
+    public function contact(Request $request, MailerInterface $mailer, TranslatorInterface $translator): Response {
         $form = $this->createForm(ContactEmailType::class, null, [
-            'attr' => ['class' => 'contact-form']
+            'attr' => ['class' => 'contact-form', 'onsubmit' => 'return formValidate();']
         ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $message = (new Email())
+                ->from($formData['email'])
+                ->to('lukacsurgay@gmail.com')
+                ->subject('Message from ' . $formData['name'])
+                ->text($formData['message'])
+            ;
+
+            try {
+                $this->addFlash('sent', $translator->trans('form.success'));
+                $mailer->send($message);
+                return $this->redirect($this->generateUrl('contact'));
+            } catch (TransportExceptionInterface $e) {
+                $this->addFlash('fail', $translator->trans('form.fail'));
+            }
+        }
+
 
         return $this->render('contact.html.twig', [
             'contact_form' => $form->createView()
--- src/Form/ContactEmailType.php
+++ src/Form/ContactEmailType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactEmailType extends AbstractType
@@ -25,8 +26,6 @@ class ContactEmailType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
+        $resolver->setDefaults([]);
     }
 }
